3ab6ee406e1dd8e59ee6f5ec9b4568c7f3d80b12
anastasia <resh@mail.ru>
2025-09-30 21:13:14 +0300

    Исправлено

--- 30.09.py
+++ 30.09.py
@@ -1,16 +1,22 @@
-first_1 = int(input("Введите начало  диапазона 1 :"))
-first_2 = int(input("Введите конец диапазона 1:"))
-second_1 = int(input("Введите начало  диапазона 2 :"))
-second_2 = int(input("Введите конец диапазона 2:"))
-third_1 = int(input("Введите начало  диапазона 3 :"))
-third_2 = int(input("Введите конец диапазона 3:"))
+min_1 = int(input("Введите начало  диапазона 1 :"))
+max_1 = int(input("Введите конец диапазона 1:"))
+min_2 = int(input("Введите начало  диапазона 2 :"))
+max_2 = int(input("Введите конец диапазона 2:"))
+min_3 = int(input("Введите начало  диапазона 3 :"))
+max_3 = int(input("Введите конец диапазона 3:"))
 
 number = int(input("Введите число :"))
-if first_1 <= number <=first_2 or second_1 <= number <= second_2 or third_1 <= number <= third_2:
-    print("Число находится в 1 диапазоне")
-    if first_1 <=number <= second_2:
-        print("Число находится в 2 диапазонах")
-        if first_1 <= number <= third_2:
-            print("Число находится в 2 диапазонах")
 
+# решаем через счетчик
+counter = 0
+if min_1>max_1:
+    min_1,max_1=max_1,min_1
+if min_2>max_2:
+    min_2,max_2 = max_2,min_2
+if min_3>max_3:
+    min_3,max_3=max_3,min_3
 
+if min_1 <=n <=max_1: counter+=1
+if min_2 <=n <=max_2: counter+=1
+if min_3 <=n <=max_3: counter+=1
+print(counter)
